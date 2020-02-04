173baab90ac76d82673b00b519bcee443cadb3a2
ShakilaZahraP <shakilazahra29@gmail.com>
2020-02-04 21:39:13 +0700

    TugasPendahuluan3

---
+++ app/src/main/java/com/example/tugaspendahuluan3/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tugaspendahuluan3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    over
+    ride fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
